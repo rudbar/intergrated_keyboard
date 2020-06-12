1101f7433c92ff09316581b6937e25630223658b
Ian Kachan <kachan.yanf@gmail.com>
2020-06-12 22:48:31 +0500

    Setup the skeleton

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,10 +16,69 @@ const Keyboard = {
 		},
 
 		init() {
+			// Создаем основные элементы для клавиатуры
+			this.elements.main = document.createElement("div");
+			this.elements.keysContainer = document.createElement("div");
 
+			// Добавление атрибутов и кнопок
+			this.elements.main.classList.add("keyboard", "1keyboard--hidden");
+			this.elements.keysContainer.classList.add("keyboard__keys");
+
+			// Добавляем созданные элементы в DOM
+			this.elements.main.appendChild(this.elements.keysContainer);
+			document.body.appendChild(this.elements.main);
 		},
 
 		_createKeys() {
 			
+			const fragment = document.createDocumentFragment();
+			const keyLayout = [
+				"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+				"q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+				"caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+				"done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+				"space"
+			];
+
+			// Функция для создания иконки
+			const createIconHTML = (icon_name) => {
+				return '<i class="material-icons">${icon_name}</i>';
+			};
+
+			keyLayout.forEach(key => {
+				const keyElement = document.createElement("button");
+				const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+
+				// Прикрепляем классы и атрибуты к дивам
+				keyElement.setAttribute("type", "button");
+				keyElement.classList.add("keyboard__key");
+
+				switch(key) {
+					case "backspace":
+					keyElement.classList.add("keyboard__key--wide");
+					keyElement.innerHTML = createIconHTML("backspace");
+				}
+			});
+
+		},
+
+		_triggerEvent(handlerName) {
+			console.log("Произошло событие: " + handlerName);
+		},
+
+		_toggleCapsLock() {
+			console.log("Нажат Caps Lock!");
+		},
+
+		open(initialValue, oninput, onclose) {
+
+		},
+
+		close() {
+
 		}
 };
+
+window.addEventListener("DOMContentLoaded", function() {
+	Keyboard.init();
+});
